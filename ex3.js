a4af2b0089379305578abcfcce0c731ae3023d96
unknown <matheus.arj@hotmail.com>
2022-07-15 22:25:42 -0300

    adicionado o exercicio3

---
+++ ex3.js
@@ -0,0 +1,7 @@
+/* Função que recebe um array com 5 números inteiros, e retorna um array com os mesmo valores na ordem inversa. */
+
+
+var numerosInversos = [1, 2, 3]
+numerosInversos.reverse();
+
+console.log(numerosInversos)
