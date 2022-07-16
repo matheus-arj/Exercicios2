2258f3f04e009ad5c69ca4cec91b33e2a00cad30
unknown <matheus.arj@hotmail.com>
2022-07-15 22:27:54 -0300

    Exercicio-teste comentado

--- ex3.js
+++ ex3.js
@@ -1,7 +1,10 @@
 /* Função que recebe um array com 5 números inteiros, e retorna um array com os mesmo valores na ordem inversa. */
 
+/* PRIMEIRO TESTE FEITO COM VARIÁVEL 
 
 var numerosInversos = [1, 2, 3]
 numerosInversos.reverse();
 
 console.log(numerosInversos)
+
+*/
