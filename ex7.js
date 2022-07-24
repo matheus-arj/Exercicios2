e95eddf829ae86b41e43f9fc022e999c05592389
unknown <matheus.arj@hotmail.com>
2022-07-23 21:33:59 -0300

    adicionado propriedade .join para retornar em string

--- ex7.js
+++ ex7.js
@@ -1,11 +1,25 @@
 /* 7 - Recebe uma frase e devolve ela com a ordem das palavras invertida */ 
-function reverseString(palavras) {
-    var newString = "";
-    for (var i = palavras.length - 1; i >= 0; i--) {
-        newString += palavras[i];
-    /*  newString = newString + palavras[i]; */
+// function reverseString(palavras) {
+//     var newString = "";
+//     for (var i = palavras.length - 1; i >= 0; i--) {
+//         newString += palavras[i];
+//     /*  newString = newString + palavras[i]; */
 
+//     }
+//     return newString;
+// }
+// console.log(reverseString('Oi eu sou o Goku'));
+
+
+function reverterOrdemPalavras(fraseinicial) {
+    var arrayDePalavras = fraseinicial.split(" ");
+    var fraserRevertida = []
+
+    for (var i = arrayDePalavras.length - 1; i >= 0; i--) {
+        fraserRevertida.push(arrayDePalavras[i])
     }
-    return newString;
+
+    return fraserRevertida = fraserRevertida.join(" ")
 }
-console.log(reverseString('Oi eu sou o Goku'));
+
+console.log(reverterOrdemPalavras('Eu sou o Goku'));
