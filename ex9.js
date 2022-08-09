7d6ae2e891a76f539c2d402d3e5136640f2767e8
unknown <matheus.arj@hotmail.com>
2022-08-09 16:46:53 -0300

    Exercicio de reajuste salarial

---
+++ ex9.js
@@ -0,0 +1,30 @@
+// relatorio de um funcionario
+// receber o valor do salario e UMA String com Nome e Sobrenome
+// Retornar um mapa/dicionario com as seguintes infs.: 
+// - salario com reajuste de 10%
+// - nome do meliante
+// - sobrenome do meliante
+
+// 1 TESTE: 
+function calculoDeReajuste(salario, nome) {
+
+    var reajuste = salario * 0.10
+    var total = salario + reajuste
+    
+    return total + nome    
+}
+
+console.log(calculoDeReajuste(3000, "Matheus Araujo"));
+
+// 2 TESTE: 
+// function calculoDeReajuste(salario, nome) {
+
+//     var reajuste = salario * 0.10
+//     var total = salario + reajuste
+    
+//     return {total, nome}    
+// }
+
+// const inforFuncionario = calculoDeReajuste(2400, "Matheus Araujo")
+
+// console.log(`Salario: ${inforFuncionario.total} - Nome: ${inforFuncionario.nome}`);
