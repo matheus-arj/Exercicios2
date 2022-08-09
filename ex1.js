b9eae4663b8aae8184a844012f6e3a4d6e86bee3
unknown <matheus.arj@hotmail.com>
2022-08-09 17:02:12 -0300

    correcao exercicio 1

--- ex1.js
+++ ex1.js
@@ -1,38 +1,41 @@
 /* 1 - Recebe base e expoente, devolve resultado da potencia */
 
-function calculationExp(base, expoente){
+// function calculationExp(base, expoente){
 
-    return Math.pow(base, expoente)
+//     return Math.pow(base, expoente)
     
-}
+// }
 
-console.log(Math.pow(2,7))
+// console.log(Math.pow(2,7))
 
 
-function calculationExp(base, expoente){
-    var base = 0
-    for (let i = 0; i <= expoente; i++)
+// function calculationExp(base, expoente){
+//     var base = 0
+//     for (let i = 0; i <= expoente; i++)
      
-    expoente **= base
+//     expoente **= base
 
 
-}
+// }
 
-console.log(2,6);
+// console.log(2,6);
 
 
-function Calcular(numero, potencia){
+function calcular(base, expoente){
 
-    var resultado = potencia;
+    var resultado = 1
 
-    for(var i = 0; i < potencia; i++){
-        resultado *= numero;
+    for (var i = 1; i < expoente; i++){
+
+        resultado = resultado * base;
      }
 
+     return resultado
+
     
 }
 
-console.log(2, 6);
+console.log(calcular(3, 4))
 
 
 
